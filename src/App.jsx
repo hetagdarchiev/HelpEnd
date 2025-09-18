77158c5a8288a183a7452d8c31129073196b6bd6
Pro100Simpleton <179834483+hetagdarchiev@users.noreply.github.com.>
2025-09-18 12:32:35 +0300

    changes code route to works site in github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import "./index.scss";
 
